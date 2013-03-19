90247e2a04ed788ae626881d266fed2c77b6a1f2
poorevil <poorevil@gmail.com>
2013-03-19 10:21:18 +0800

    init

---
+++ BabyPictorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BabyPictorial
+//
+//  Created by han chao on 13-3-19.
+//  Copyright (c) 2013年 taoxiaoxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BabyPictorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BabyPictorial
+//
+//  Created by han chao on 13-3-19.
+//  Copyright (c) 2013年 taoxiaoxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
